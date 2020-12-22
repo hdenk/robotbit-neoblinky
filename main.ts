36838ced2c74334409072309fa6ff76de6bab5bc
hdenk <68135+hdenk@users.noreply.github.com>
2020-12-22 14:47:41 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showIcon(IconNames.Heart)
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+basic.forever(function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(1000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(1000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(1000)
+})
